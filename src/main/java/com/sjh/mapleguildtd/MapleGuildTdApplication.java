dfd3471d6a609c7734faea3a1b7b08d263b4372c
tjwlgns15 <tjwlgns35@naver.com>
2026-05-22 14:47:43 +0900

    first commit

---
+++ src/main/java/com/sjh/mapleguildtd/MapleGuildTdApplication.java
@@ -0,0 +1,13 @@
+package com.sjh.mapleguildtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleGuildTdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleGuildTdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjh/mapleguildtd/MapleGuildTdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjh.mapleguildtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleGuildTdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
